86fc4d9f80d27b2df67362a66dca24a356e66a40
olliecrasta <68199773+olliecrasta@users.noreply.github.com>
2020-09-07 12:18:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,16 +5,16 @@ console.log(1);
 
 var engine, world, box1,ground,pig1;
 function setup() {
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(1200,400);
   console.log(1);
   engine = Engine.create();
   world = engine.world;
   box1 = new Box(200,300,50,50);
   box2 = new Box(240,100,50,100);
  
-  ground = new Ground(200, height ,400,10);
+  ground = new Ground(600, height ,1200,20);
   pig1 = new Pig(200,200);
-  log1 = new Log(240,100,60,-40)
+  log1 = new Log(100,300,100,90);
 }
 
 function draw() {
